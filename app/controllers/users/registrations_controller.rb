4bf1c03d092ecc556299589c97faad6ead0eb84a
aomoe <arjceimx0311@gmail.com>
2025-08-15 09:40:37 +0900

    マイページ作成

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
-  def sign_up(resource_name, resource)
-
-  end
+  def sign_up(resource_name, resource);end
 
   def after_sign_up_path_for(resource)
     new_user_session_path
